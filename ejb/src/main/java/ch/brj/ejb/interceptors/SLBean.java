3b3b965505e7186d55546baf82727a5425835457
jakob braeuchi <jbraeuchi@gmx.ch>
2016-10-12 22:50:12 +0200

    Resource from env

--- ejb/src/main/java/ch/brj/ejb/interceptors/SLBean.java
+++ ejb/src/main/java/ch/brj/ejb/interceptors/SLBean.java
@@ -1,8 +1,12 @@
 package ch.brj.ejb.interceptors;
 
+import javax.annotation.Resource;
+import javax.ejb.EJBException;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import java.util.Date;
 
 /**
@@ -12,17 +16,28 @@ import java.util.Date;
 @LocalBean
 public class SLBean {
 
+    @Resource(name = "greeting")
+    String greeting = "Hello from Bean";
+
     @Interceptors({InterceptorA.class})
     public String greeting(String name) {
+        try {
+            Context ctx = new InitialContext();
+            String greeting2 = (String) ctx.lookup("java:comp/env/greeting");
+            System.out.println("*** Greeting 2: " + greeting2);
+        } catch (Exception e) {
+            throw new EJBException(e);
+        }
+
         Date theCurrentTime = new Date();
-        String theMessage = "Hello " + name + " from interceptors.SLBean. The time is now: " + theCurrentTime;
+        String theMessage = greeting + " " + name + " from interceptors.SLBean. The time is now: " + theCurrentTime;
         return theMessage;
     }
 
     @Interceptors({InterceptorB.class})
     public String greeting2(String name) {
         Date theCurrentTime = new Date();
-        String theMessage = "Hello " + name + " from interceptors.SLBean. The time is now: " + theCurrentTime;
+        String theMessage = greeting + " " + name + " from interceptors.SLBean. The time is now: " + theCurrentTime;
         return theMessage;
     }
 
