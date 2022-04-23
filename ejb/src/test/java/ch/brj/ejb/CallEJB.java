122eb81ed348b746f254b199edd65cd987858a53
jakob braeuchi <jbraeuchi@gmx.ch>
2022-04-23 14:16:24 +0200

    wildfly client

--- ejb/src/test/java/ch/brj/ejb/CallEJB.java
+++ ejb/src/test/java/ch/brj/ejb/CallEJB.java
@@ -27,13 +27,22 @@ public class CallEJB {
     // ejb:<app-name>/<module-name>/<distinct-name>/<bean-name>!<fully-qualified-classname-of-the-remote-interface>?stateful
     //
     // see https://blog.akquinet.de/2014/09/26/jboss-eap-wildfly-three-ways-to-invoke-remote-ejbs/
+    //
+    //    java:global/ear-0.0.1-SNAPSHOT/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote
+    //    java:app/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote
+    //    java:module/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote
+    //    java:jboss/exported/ear-0.0.1-SNAPSHOT/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote
+    //    ejb:ear-0.0.1-SNAPSHOT/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote
+    //    java:global/ear-0.0.1-SNAPSHOT/ejb/StatelessSessionRemoteBean
+    //    java:app/ejb/StatelessSessionRemoteBean
+    //    java:module/StatelessSessionRemoteBean
 
     @Test
     public void test_ejbNaming() throws Exception {
         final Properties env = new Properties();
-        env.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-
-        String name1 = "ejb:EJBTest/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote";
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+        env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+        String name1 = "ejb:ear-0.0.1-SNAPSHOT/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote";
 
         Context ctx = new InitialContext(env);
         StatelessSessionRemote bean = (StatelessSessionRemote) ctx.lookup(name1);
@@ -46,9 +55,8 @@ public class CallEJB {
         final Properties env = new Properties();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
         env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
-        env.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
 
-        String name1 = "/EJBTest/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote";
+        String name1 = "/ear-0.0.1-SNAPSHOT/ejb/StatelessSessionRemoteBean!ch.brj.ejb.StatelessSessionRemote";
 
         Context ctx = new InitialContext(env);
         StatelessSessionRemote bean = (StatelessSessionRemote) ctx.lookup(name1);
@@ -62,9 +70,10 @@ public class CallEJB {
     @Test
     public void test_SLRemote() throws Exception {
         final Properties env = new Properties();
-        env.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+        env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
 
-        String name1 = "ejb:EJBTest/ejb/SLMultiBean!ch.brj.ejb.multi.SLRemote";
+        String name1 = "ejb:ear-0.0.1-SNAPSHOT/ejb/SLMultiBean!ch.brj.ejb.multi.SLRemote";
 
         Context ctx = new InitialContext(env);
         SLRemote bean = (SLRemote) ctx.lookup(name1);
